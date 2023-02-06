460bd1f1c6634924df5c3e8cfd805a6f303998df
Marcos Vinicius <marcosvinicius.lucena27@gmail.com>
2023-02-05 21:25:55 -0300

    Animações
    Animações adicionadas no site.

--- Projeto-integrador/assets/js/script.js
+++ Projeto-integrador/assets/js/script.js
@@ -20,3 +20,23 @@ var btn = $("#back-to-top");
 btn.click(function () {
   $("html, body").animate({ scrollTop: 0 }, "slow");
 });
+
+const elementos = document.querySelectorAll("[data-anima]");
+const animacaoClass = "animacao";
+
+function animaScroll() {
+  const topoPaginaNaJanela = window.pageYOffset + (window.innerHeight * 3) / 4;
+  elementos.forEach(function (elemento) {
+    if (topoPaginaNaJanela > elemento.offsetTop) {
+      elemento.classList.add(animacaoClass);
+    } else {
+      elemento.classList.remove(animacaoClass);
+    }
+  });
+}
+
+if (elementos.length) {
+  window.addEventListener("scroll", function () {
+    animaScroll();
+  });
+}
