452925b5eed617882344f9c71cc130f002f3f478
Marcos Vinicius <marcosvinicius.lucena27@gmail.com>
2023-02-06 09:07:16 -0300

    Adição de preloader na página

---
+++ Projeto-integrador/assets/js/scriptpreLoader.js
@@ -0,0 +1,7 @@
+//<![CDATA[
+$(window).on("load", function () {
+  $("#preloader .inner").fadeOut();
+  $("#preloader").delay(350).fadeOut("slow");
+  $("body").delay(350).css({ overflow: "visible" });
+});
+//]]>
