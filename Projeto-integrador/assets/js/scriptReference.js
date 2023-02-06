b0de66c4841d7458fd292a66b2c8673db4aa56e4
Marcos Vinicius <marcosvinicius.lucena27@gmail.com>
2023-02-06 08:45:39 -0300

    #1 Correção no onclick

--- Projeto-integrador/assets/js/scriptReference.js
+++ Projeto-integrador/assets/js/scriptReference.js
@@ -1,6 +1,6 @@
 function reference() {
   vid = document.getElementById("video");
-  vid.play();
   const video = document.querySelector("#video");
   video.requestPictureInPicture();
+  vid.play();
 }
